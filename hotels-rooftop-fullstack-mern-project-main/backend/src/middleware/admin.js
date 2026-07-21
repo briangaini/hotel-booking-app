24176d48dc89e79df5b19163cfe2d0e79461aa34
MacBook Air <macbookair@MacBook-Air.lan>
2026-07-21 02:53:06 -0400

    Remove duplicate project folder and zip

--- hotels-rooftop-fullstack-mern-project-main/backend/src/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.role !== 'admin') {
-        return res.status(403).json({ success: false, message: 'You are not authorized to perform this action.' });
-    }
-    next();
-};
-
-module.exports = isAdmin;
